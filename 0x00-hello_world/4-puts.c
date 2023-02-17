26ca099f243fc74c620fb0880464a0082d21d78d
Godfred <godfredm127@gmail.com>
2023-02-16 16:35:50 -0800

    task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main -Print a line of code puts
- *
- * Return: 0
+ * main - Entry
+ * Return: Always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like buildinig a multilingual puzzle")
+
+	puts("\"Programming is like buildinig a multilingual puzzle");
 	return (0);
 }
