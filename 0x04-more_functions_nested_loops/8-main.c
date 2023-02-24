e07e7d3afc7da2e0e5dda367e864fa4ece7c8638
Godfred <godfredm127@gmail.com>
2023-02-23 17:02:33 -0800

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	if (size <= 0)
+
+	print_square(2);
+	print_square(10);
+	print_square(0);
+	return (0);
+}
